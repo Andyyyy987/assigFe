697233741ee5faf5e9bf67e6259bbe63ab661180
Andyyyy987 <am38387@gmail.com>
2024-02-27 09:13:56 +0000

    updates to app/sign up/fetch

--- assigFe/src/Components/logOrSign/LogOrSign.jsx
+++ assigFe/src/Components/logOrSign/LogOrSign.jsx
@@ -1,1 +1,17 @@
-junh
+import React from 'react';
+
+import Login from "../login/Login";
+import Signup from '../signup/Signup';
+
+import "./LogOrSign.css";
+
+const LogOrSign = () => {
+  return (
+    <div className='logorsign-wrapper'>
+        <Signup />
+        <Login />
+    </div>
+  )
+};
+
+export default LogOrSign
